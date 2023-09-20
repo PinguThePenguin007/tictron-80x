9b340fffc8ce111bfb54aa0b39a09ac7bb8672e6
PinguThePenguin007 <mishastep7@gmail.com>
2023-09-21 00:34:55 +0300

    improve addobjectstoscene()

--- renderer_functions/add_objects_to_scene.lua
+++ renderer_functions/add_objects_to_scene.lua
@@ -2,14 +2,16 @@ function Renderer.addObjectsToScene(list,customscene,depth,AddOrigins)
 
 	depth=depth or 5
 
-	for key,sublist in pairs(list) do; if type(sublist)~="table" then return 0; else
+	for _,sublist in pairs(list) do
+		if type(sublist)=="table" and sublist.RenderObject~=false and sublist.RenderTable~=false then
 
-		if sublist.RenderObject~=false and (sublist.RenderObject~=nil or sublist.mesh~=nil) then
-			Renderer.addSingleObject(sublist,customscene,AddOrigins)
-		else
-			if depth>0 then Renderer.addObjectsToScene(sublist,customscene,AddOrigins,depth-1) end
+			if sublist.RenderObject~=nil or sublist.mesh~=nil then
+				Renderer.addSingleObject(sublist,customscene,AddOrigins)
+			else
+				if depth>0 then Renderer.addObjectsToScene(sublist,customscene,AddOrigins,depth-1) end
+			end
+
 		end
-
-	end; end
+	end
 
 end
