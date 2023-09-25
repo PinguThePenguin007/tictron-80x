77541d7f5f0578af1a4b62ca448531d59b5b9351
PinguThePenguin007 <mishastep7@gmail.com>
2023-09-26 01:06:17 +0300

    fix addobjectstoscene()
    now it can properly see a single object as an input

--- renderer_functions/add_objects_to_scene.lua
+++ renderer_functions/add_objects_to_scene.lua
@@ -2,16 +2,16 @@ function Renderer.addObjectsToScene(list,customscene,depth,AddOrigins)
 
 	depth=depth or 5
 
-	for _,sublist in pairs(list) do
-		if type(sublist)=="table" and sublist.RenderObject~=false and sublist.RenderTable~=false then
+	if type(list)=="table" and list.RenderObject~=false and list.RenderTable~=false then
 
-			if sublist.RenderObject~=nil or sublist.mesh~=nil then
-				Renderer.addSingleObject(sublist,customscene,AddOrigins)
-			else
+		if list.RenderObject~=nil or list.mesh~=nil then
+			Renderer.addSingleObject(list,customscene,AddOrigins)
+		else
+			for _,sublist in pairs(list) do
 				if depth>0 then Renderer.addObjectsToScene(sublist,customscene,depth-1,AddOrigins) end
 			end
-
 		end
+
 	end
 
 end
