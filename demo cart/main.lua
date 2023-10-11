603a2ab9a08f32798b514c08cf2b518d6a147122
PinguThePenguin007 <mishastep7@gmail.com>
2023-10-11 17:40:50 +0300

    add UV support to simpleclipscene()

--- demo cart/main.lua
+++ demo cart/main.lua
@@ -173,33 +173,31 @@ NoClip=false
 function TIC()
 	T=(T and T+1) or 0
 
-Zoffset=0
-
 	Markinit()
 	TotalTime=time()
 
 	Renderer.resetScene()
-	--Gui_Scene={}
-	--Renderer.resetScene(Gui_Scene,Rscene.camera)
+	Gui_Scene={}
+	Renderer.resetScene(Gui_Scene,Rscene.camera)
 
 	Renderer.addObjectsToScene(ObjectList)
 
-	--Renderer.addObjectsToScene(GuiObjects,Gui_Scene)
+	Renderer.addObjectsToScene(GuiObjects,Gui_Scene)
 
 	Renderer.clipObjects()
 
 	local totalvertices=
 	Renderer.transformVerts()
-	--+
-	--Renderer.transformVerts(Gui_Scene)
+	+
+	Renderer.transformVerts(Gui_Scene)
 
 	Mark("VertDumpTime")
 
 	Renderer.addDrawElements()
-	--Renderer.addDrawElements(Gui_Scene)
+	Renderer.addDrawElements(Gui_Scene)
 
 	Renderer.replaceLabels()
-	--Renderer.replaceLabels(Gui_Scene)
+	Renderer.replaceLabels(Gui_Scene)
 	Mark("TriDumpTime")
 
 -- uncomment to enable display of object origins and their "sizes"
@@ -215,19 +213,20 @@ Zoffset=0
 --]]
 
 	Renderer.simpleClipScene()
+	Renderer.simpleClipScene(Gui_Scene)
 
 	--Renderer.clipScene()
 	--Renderer.clipScene(Gui_Scene)
 	Mark("TriClipTime")
 
 	Renderer.sortScene()
-	--Renderer.sortScene(Gui_Scene)
+	Renderer.sortScene(Gui_Scene)
 	Mark("TriSortTime")
 
 	cls(11)
 
 	Renderer.drawScene()
-	--Renderer.drawScene(Gui_Scene)
+	Renderer.drawScene(Gui_Scene)
 
 	Mark("TriDrawTime")
 
--- renderer_functions/simple_clip_scene.lua
+++ renderer_functions/simple_clip_scene.lua
@@ -4,66 +4,187 @@ function Renderer.simpleClipScene(customscene)
 	local vertexdump=scene.vertexdump
 	local drawdump=scene.drawdump
 
+	local table_remove,uv3dlerp=table.remove,RendererLib.UV3DLerp
+
 	if vertexdump.clippedverts==nil then vertexdump.clippedverts={} end
 	local clippedverts=vertexdump.clippedverts
 
+	local near_distance=.1
+
 	local verts_behind={nil,nil}
+	local verts_ahead={nil,nil}
 
 	for eid=#drawdump,1,-1 do
 		local element=drawdump[eid]
 		local NofVerts=element.nofverts
 
 		if NofVerts==3 then
-			if not (element[1].z>0 or element[2].z>0 or element[3].z>0) then table.remove(drawdump,eid)
-			elseif element[1].z<0 or element[2].z<0 or element[3].z<0 then
+			local element_1_z,element_2_z,element_3_z=element[1].z,element[2].z,element[3].z
 
-			local verts_behind_len=0
+			if not (element_1_z>near_distance or element_2_z>near_distance or element_3_z>near_distance) then
+				table_remove(drawdump,eid)
+			elseif element_1_z<near_distance or element_2_z<near_distance or element_3_z<near_distance then
 
-				for i=1,3 do; if element[i].z<0 then
-					verts_behind[verts_behind_len+1]=i verts_behind_len=verts_behind_len+1
-				end; end
+
+				local hasuv=element.data.uv
+
+				local uv
+				if hasuv then uv=element.uv end
+
+
+				local verts_behind_len,verts_ahead_len=0,0
+
+				for i=1,3 do
+					if element[i].z<near_distance then
+						verts_behind[verts_behind_len+1]=i verts_behind_len=verts_behind_len+1
+					else
+						verts_ahead[verts_ahead_len+1]=i verts_ahead_len=verts_ahead_len+1
+					end
+				end
+
 
 				if verts_behind_len==1 then
-					local v1,v2,v3=element[verts_behind[1]],
+					local v1index,v2index,v3index=
+					 verts_behind[1],
 
-					element[((verts_behind[1]-2)%3)+1],
-					element[((verts_behind[1])%3)+1]
+					 verts_ahead[1],
+					 verts_ahead[2]
+
+					local v1,v2,v3=
+					 element[v1index],element[v2index],element[v3index]
+
+					local v1indexM2,v2indexM2,v3indexM2=v1index*2,v2index*2,v3index*2
+					local v1U_index,v1V_index,v2U_index,v2V_index,v3U_index,v3V_index=
+					 v1indexM2-1,v1indexM2,
+					 v2indexM2-1,v2indexM2,
+					 v3indexM2-1,v3indexM2
 
 
-					local new_vert=RendererLib.UV3DLerp(v1,v2,nil,nil,nil,nil,v1.z/(v1.z-v2.z),nil,0.001)
+
+					local new_vert,nvU,nvV=uv3dlerp(v1,v2,
+					(v1.z-near_distance)/(v1.z-v2.z),near_distance,
+					 hasuv,
+					 hasuv and uv[v1U_index],
+					 hasuv and uv[v1V_index],
+					 hasuv and uv[v2U_index],
+					 hasuv and uv[v2V_index])
 
 					clippedverts[#clippedverts+1]=new_vert
 
-					element[verts_behind[1]]=RendererLib.UV3DLerp(v1,v3,nil,nil,nil,nil,v1.z/(v1.z-v3.z),nil,0.001)
+					local v1U,v1V
+					element[v1index],v1U,v1V=uv3dlerp(v1,v3,
+					(v1.z-near_distance)/(v1.z-v3.z),near_distance,
+					 hasuv,
+					 hasuv and uv[v1U_index],
+					 hasuv and uv[v1V_index],
+					 hasuv and uv[v3U_index],
+					 hasuv and uv[v3V_index])
 
-					clippedverts[#clippedverts+1]=element[verts_behind[1]]
+					clippedverts[#clippedverts+1]=element[v1index]
 
-					drawdump[#drawdump+1]={element[verts_behind[1]],v2,new_vert;  type="t",nofverts=3,data=element.data,uv=element.uv,object=element.object}
+					local newtri_uv
+					if hasuv then
+						local new_uv={nil,nil,nil,nil,nil,nil}
+
+						new_uv[v1U_index]=v1U
+						new_uv[v1V_index]=v1V
+						new_uv[v2U_index]=uv[v2U_index]
+						new_uv[v2V_index]=uv[v2V_index]
+						new_uv[v3U_index]=uv[v3U_index]
+						new_uv[v3V_index]=uv[v3V_index]
+
+						element.uv=new_uv
+
+						newtri_uv={
+						 v1U,v1V,
+						 uv[v2U_index],
+						 uv[v2V_index],
+						 nvU,nvV
+						}
+
+					end
+
+					drawdump[#drawdump+1]={element[v1index],v2,new_vert;  type="t",nofverts=3,data=element.data,uv=newtri_uv,object=element.object}
 
 				else
+					local v1index,v2index,v3index=
+					 verts_behind[1],
+					 verts_behind[2],
+
+					 verts_ahead[1]
+
 					local v1,v2,v3=
-					element[verts_behind[1]],element[verts_behind[2]],
+					 element[v1index],element[v2index],element[v3index]
 
-					((verts_behind[1]%3)+1==verts_behind[2] and
-					element[((verts_behind[1]-2)%3)+1]) or
-					element[((verts_behind[1])%3)+1]
+					local v1indexM2,v2indexM2,v3indexM2=v1index*2,v2index*2,v3index*2
+					local v1U_index,v1V_index,v2U_index,v2V_index,v3U_index,v3V_index=
+					 v1indexM2-1,v1indexM2,
+					 v2indexM2-1,v2indexM2,
+					 v3indexM2-1,v3indexM2
 
 
-					element[verts_behind[1]]=RendererLib.UV3DLerp(v1,v3,nil,nil,nil,nil,v1.z/(v1.z-v3.z),nil,0.001)
 
-					clippedverts[#clippedverts+1]=element[verts_behind[1]]
+					local v1U,v1V
+					element[v1index],v1U,v1V=uv3dlerp(v1,v3,
+					(v1.z-near_distance)/(v1.z-v3.z),near_distance,
+					 hasuv,
+					 hasuv and uv[v1U_index],
+					 hasuv and uv[v1V_index],
+					 hasuv and uv[v3U_index],
+					 hasuv and uv[v3V_index])
 
-					element[verts_behind[2]]=RendererLib.UV3DLerp(v2,v3,nil,nil,nil,nil,v2.z/(v2.z-v3.z),nil,0.001)
+					clippedverts[#clippedverts+1]=element[v1index]
 
-					clippedverts[#clippedverts+1]=element[verts_behind[2]]
+					local v2U,v2V
+					element[v2index],v2U,v2V=uv3dlerp(v2,v3,
+					(v2.z-near_distance)/(v2.z-v3.z),near_distance,
+					 hasuv,
+					 hasuv and uv[v2U_index],
+					 hasuv and uv[v2V_index],
+					 hasuv and uv[v3U_index],
+					 hasuv and uv[v3V_index])
+
+					clippedverts[#clippedverts+1]=element[v2index]
+
+					if hasuv then
+						local new_uv={nil,nil,nil,nil,nil,nil}
+
+						new_uv[v1U_index]=v1U
+						new_uv[v1V_index]=v1V
+						new_uv[v2U_index]=v2U
+						new_uv[v2V_index]=v2V
+						new_uv[v3U_index]=uv[v3U_index]
+						new_uv[v3V_index]=uv[v3V_index]
+
+						element.uv=new_uv
+					end
+
 				end
 
 			end
 
 		elseif NofVerts==2 then
-			table.remove(drawdump,eid)
+
+			local P1,P2=element[1],element[2]
+
+			if P1.z<near_distance and P2.z<near_distance then
+				table_remove(drawdump,eid)
+
+			elseif P1.z<near_distance or P2.z<near_distance then
+
+				if P1.z<near_distance then
+					P1=uv3dlerp(P1,P2, (P1.z-near_distance)/(P1.z-P2.z),near_distance)
+					clippedverts[#clippedverts+1]=P1
+				else
+					P2=uv3dlerp(P2,P1, (P2.z-near_distance)/(P2.z-P1.z),near_distance)
+					clippedverts[#clippedverts+1]=P2
+				end
+
+					element[1],element[2]=P1,P2
+			end
+
 		else
-			if element[1].z<0 then table.remove(drawdump,eid) end
+			if element[1].z<near_distance then table_remove(drawdump,eid) end
 		end
 
 	end
